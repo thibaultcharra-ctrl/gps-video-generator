9832fe458a186458a9ab66a7511e6cb1e00a7079
Thibault Charra <thibault.charra@gmail.com>
2025-12-01 11:13:18 +0100

    strava connect5

--- app.py
+++ app.py
@@ -118,7 +118,7 @@ with st.sidebar:
         client_id = st.text_input(
             "Client ID",
             value=st.session_state.get('strava_client_id', '187965'),
-            type="text"
+            type="default"
         )
         client_secret = st.text_input(
             "Client Secret",
